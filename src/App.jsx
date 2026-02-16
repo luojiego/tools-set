2cf87ca444a8e9482b46d85041a0ec3413aa029f
罗杰 <xiaoyaojie005@gmail.com>
2026-02-16 19:52:12 +0800

    fix: 修复刷新404后跳转主页问题，使用sessionStorage保存路径后跳转

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
 import Home from './pages/Home'
 import PasswordPage from './pages/PasswordPage'
 import CryptoPage from './pages/CryptoPage'
@@ -7,18 +8,34 @@ import JsonPage from './pages/JsonPage'
 import TimePage from './pages/TimePage'
 import IpPage from './pages/IpPage'
 
+function AppContent() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const redirect = sessionStorage.getItem('redirect')
+    if (redirect) {
+      sessionStorage.removeItem('redirect')
+      navigate(redirect)
+    }
+  }, [navigate])
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/tools/password" element={<PasswordPage />} />
+      <Route path="/tools/crypto" element={<CryptoPage />} />
+      <Route path="/tools/id" element={<IdPage />} />
+      <Route path="/tools/ip" element={<IpPage />} />
+      <Route path="/tools/time" element={<TimePage />} />
+      <Route path="/tools/json" element={<JsonPage />} />
+    </Routes>
+  )
+}
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tools/password" element={<PasswordPage />} />
-        <Route path="/tools/crypto" element={<CryptoPage />} />
-        <Route path="/tools/id" element={<IdPage />} />
-        <Route path="/tools/ip" element={<IpPage />} />
-        <Route path="/tools/time" element={<TimePage />} />
-        <Route path="/tools/json" element={<JsonPage />} />
-      </Routes>
+      <AppContent />
     </BrowserRouter>
   )
 }
